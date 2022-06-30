00dee70bb2e6bb50d938779072b617f6cee676d1
辛山 <1024459323@qq.com>
2022-06-30 11:27:37 +0800

    up readme

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ const photoswipe = {}
 photoswipe.install = function (Vue, options) {
   const Constructor = Vue.extend(photoswipeComponent)
   const instance = new Constructor({options})
-  console.log(instance);
   instance.$mount(document.createElement('div'))
   document.body.appendChild(instance.$el)
   Vue.prototype.$previewImage = (index, list)=>{
     instance.preview(index,list);
   }
-
 }
 export default photoswipe
